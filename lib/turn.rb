f933d1b47185a18b867bd15d2e0068aeec3a8bfb
proactive-callee-1012 <shaemc22@gmail.com>
2021-01-04 17:01:22 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,9 +1,32 @@
-display_board(board)
+def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
+
+def valid_move?(board, index)
+
+def between?(index)
+  if index.between?(0,8) ==true
+    return true
+  else
+    return false
+end
+end
+def position_taken?(board, index)
+  if board[index] =="" || board[index] ==" " || board[index] == nil
+    return false
+  else 
+    return true
+end
+end
+if position_taken?(board, index) == false && between?(index) == true
+  return true
+else
+  return false || nil
+end
+end
 
 def move(board, index, character = "X")
   board[index] = character
