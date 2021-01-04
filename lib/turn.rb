7138b4ab69eeff86dc4cf8b05c213256e056930e
proactive-callee-1012 <shaemc22@gmail.com>
2021-01-04 17:03:37 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -28,7 +28,10 @@ else
   return false || nil
 end
 end
-
+def input_to_index(user_input)
+  user_input = user_input.to_i 
+  user_input - 1
+end
 def move(board, index, character = "X")
   board[index] = character
 end
